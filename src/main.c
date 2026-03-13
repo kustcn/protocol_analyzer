db18204983b391151bf0d7f5bfe8512a13884a35
Wei Shoulin <weishoulin@astrolab.cn>
2026-03-13 11:33:57 +0800

    feat(main): add network interface listing functionality

--- src/main.c
+++ src/main.c
@@ -8,17 +8,18 @@
 void print_usage(const char *program_name) {
     printf("用法: %s [选项]\n", program_name);
     printf("选项:\n");
-    printf("  -i, --interface <设备>   指定网络接口 (如 eth0, en0)\n");
+    printf("  -i, --interface <设备|list> 指定网络接口 (如 eth0, en0) 或 'list' 列出所有接口\n");
     printf("  -f, --filter <过滤器>    BPF过滤器表达式 (如 'tcp', 'udp', 'port 80')\n");
     printf("  -c, --count <数量>       捕获数据包数量 (默认无限)\n");
-    printf("  -t, --timeout <毫秒>     超时时间 (默认1000ms)\n");
+    printf("  -t, --timeout <毫秒>    超时时间 (默认1000ms)\n");
     printf("  -v, --verbose            详细输出模式\n");
     printf("  -h, --help               显示此帮助信息\n");
     printf("\n示例:\n");
-    printf("  %s -i eth0 -c 10              捕获eth0上10个数据包\n", program_name);
-    printf("  %s -f 'tcp' -c 5             捕获5个TCP数据包\n", program_name);
-    printf("  %s -f 'port 80' -v           捕获HTTP流量并显示详细信息\n", program_name);
-    printf("  %s -f 'tcp and port 443'     捕获HTTPS流量\n", program_name);
+    printf("  %s -i list                  列出所有可用网络接口\n", program_name);
+    printf("  %s -i eth0 -c 10            捕获eth0上10个数据包\n", program_name);
+    printf("  %s -f 'tcp' -c 5            捕获5个TCP数据包\n", program_name);
+    printf("  %s -f 'port 80' -v          捕获HTTP流量并显示详细信息\n", program_name);
+    printf("  %s -f 'tcp and port 443'   捕获HTTPS流量\n", program_name);
 }
 
 void print_banner(void) {
@@ -35,6 +36,46 @@ void print_banner(void) {
 
 void print_separator(void) {
     printf("------------------------------------------------------------\n");
+}
+
+void list_network_interfaces(void) {
+    pcap_if_t *alldevs;
+    char errbuf[PCAP_ERRBUF_SIZE];
+    
+    printf("\n可用网络接口:\n");
+    print_separator();
+    
+    if (pcap_findalldevs(&alldevs, errbuf) == -1) {
+        fprintf(stderr, "获取网络接口失败: %s\n", errbuf);
+        return;
+    }
+    
+    int if_count = 0;
+    for (pcap_if_t *dev = alldevs; dev != NULL; dev = dev->next) {
+        printf("  * %s", dev->name);
+        
+        if (dev->description) {
+            printf(" - %s", dev->description);
+        }
+        printf("\n");
+        
+        if (dev->addresses) {
+            for (pcap_addr_t *addr = dev->addresses; addr != NULL; addr = addr->next) {
+                if (addr->addr->sa_family == AF_INET) {
+                    char ipstr[INET_ADDRSTRLEN];
+                    struct sockaddr_in *sin = (struct sockaddr_in *)addr->addr;
+                    inet_ntop(AF_INET, &sin->sin_addr, ipstr, INET_ADDRSTRLEN);
+                    printf("    地址: %s\n", ipstr);
+                }
+            }
+        }
+        if_count++;
+    }
+    
+    print_separator();
+    printf("共找到 %d 个接口\n", if_count);
+    
+    pcap_freealldevs(alldevs);
 }
 
 void print_timestamp(const struct pcap_pkthdr *hdr) {
@@ -72,6 +113,11 @@ int main(int argc, char *argv[]) {
     while ((opt = getopt_long(argc, argv, "i:f:c:t:vh", long_options, &option_index)) != -1) {
         switch (opt) {
             case 'i':
+                if (strcmp(optarg, "list") == 0) {
+                    print_banner();
+                    list_network_interfaces();
+                    return 0;
+                }
                 config.device = optarg;
                 break;
             case 'f':
