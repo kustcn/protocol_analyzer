1aa18a19c5bdc432acddf477b3186b982aaf71a0
Shoulin Wei <weishoulin@kust.edu.cn>
2026-03-30 13:47:40 +0800

    feat(ethernet): add frame length and FCS parsing support

--- src/ethernet_parse.c
+++ src/ethernet_parse.c
@@ -3,8 +3,6 @@
 #include "utils.h"
 #include <stdio.h>
 
-#define ETH_HEADER_LEN 14
-
 int parse_ethernet(const uint8_t *packet, int length, EthernetHeader *eth) {
     if (length < ETH_HEADER_LEN) {
         return -1;
@@ -13,6 +11,16 @@ int parse_ethernet(const uint8_t *packet, int length, EthernetHeader *eth) {
     memcpy(eth->dhost, packet, 6);
     memcpy(eth->shost, packet + 6, 6);
     eth->ether_type = (packet[12] << 8) | packet[13];
+
+    // Record data length (total frame length minus Ethernet header)
+    eth->frame_len = length;
+    eth->data_len = length - ETH_HEADER_LEN;
+
+    // Note: FCS is typically stripped by NIC before passing to libpcap
+    // If present at the end of packet, copy it
+    if (length >= ETH_HEADER_LEN + ETH_FCS_LEN) {
+        memcpy(eth->fcs, packet + length - ETH_FCS_LEN, ETH_FCS_LEN);
+    }
 
     return ETH_HEADER_LEN;
 }
@@ -25,6 +33,11 @@ void print_ethernet_info(const EthernetHeader *eth) {
     format_mac(eth->dhost, dst_mac);
 
     printf("[以太网] 源MAC: %s -> 目标MAC: %s\n", src_mac, dst_mac);
+    printf("        数据长度: %d bytes\n", eth->data_len);
+    if (eth->frame_len >= ETH_HEADER_LEN + ETH_FCS_LEN) {
+        printf("        FCS: 0x%02x%02x%02x%02x\n",
+               eth->fcs[0], eth->fcs[1], eth->fcs[2], eth->fcs[3]);
+    }
 
     switch (eth->ether_type) {
         case ETH_TYPE_IPV4:
--- src/ethernet_parse.h
+++ src/ethernet_parse.h
@@ -3,10 +3,16 @@
 
 #include <stdint.h>
 
+#define ETH_HEADER_LEN 14
+#define ETH_FCS_LEN 4
+
 typedef struct {
     uint8_t  dhost[6];
     uint8_t  shost[6];
     uint16_t ether_type;
+    uint8_t  fcs[4];
+    int      frame_len;
+    int      data_len;
 } EthernetHeader;
 
 int parse_ethernet(const uint8_t *packet, int length, EthernetHeader *eth);
